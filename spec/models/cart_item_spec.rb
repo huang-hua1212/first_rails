16edcb3fabd755e488c83fdb2d9563e240b6da8f
huang-hua1212 <hahahazzz123@gmail.com>
2022-03-23 22:12:35 +0800

    commit on home_issue2

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 # homehomehome
 # homehomehome
 # homehomehome
+# homehomehome2
+# homehomehome2
 
 RSpec.describe CartItem, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
